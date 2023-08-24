3f36b288761197ca382af26531da0f4c179db70d
Nikita <32540777+nivanown@users.noreply.github.com>
2023-08-24 20:06:34 +0500

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+﻿/*- question-modal -*/
+$('#btn-question').click(function() {
+  $('.question-modal').addClass('show');
+  $('body').addClass('o-hidden');
+});
+
+$('.question-modal__close-btn').click(function() {
+  $('.question-modal').removeClass('show');
+  $('body').removeClass('o-hidden');
+});
